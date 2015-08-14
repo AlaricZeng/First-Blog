9db922d43f348e6a6aebd96cf80128ad6d0f86b9
AlaricZeng <zyj1994210@gmail.com>
2015-08-14 09:41:35 +0800

    change comment function
    did some change in the comment function to show the nested comments

--- Alaricbg/functions.php
+++ Alaricbg/functions.php
@@ -2,20 +2,22 @@
 function advanced_comment($comment, $args, $depth) 
 {
    $GLOBALS['comment'] = $comment; ?>
-        <div class="gravatar"> <?php if (function_exists('get_avatar') && get_option('show_avatars')) { echo get_avatar($comment, 48); } ?>
-  </div>
-        <div id="comment-author">
-                    <?php printf(__('<cite class="author_name">%s</cite>'), get_comment_author_link()); ?>
+   <div class="comment-container">
+        <div class="gravatar"> 
+			<?php echo get_avatar($comment);?>
         </div>
-        <div id="comment-time">发表于：<?php echo get_comment_time('Y-m-d H:i'); ?></div><?php edit_comment_link('修改'); ?>
+        <span class="comment-author">
+            <?php echo get_comment_author_link(); ?>说道：
+        </span>
+        <div class="comment-time">
+			<?php echo get_comment_date(); ?><?php echo get_comment_time(); ?>
+            <span class="edit-comment"><?php edit_comment_link('修改'); ?></span>
         </div>
-        <div id"comment-content">
-                <?php if ($comment->comment_approved == '0') : ?>
-                    <em>你的评论正在审核，稍后会显示出来！</em><br />
-        <?php endif; ?>
-        <?php comment_text(); ?>
-            </div>
-		<div id="comment-reply">
+        <div class="comment-content">
+			<?php echo comment_text(); ?>
+        </div>
+		<div class="comment-reply">
         	<?php comment_reply_link(array_merge( $args, array('reply_text' => '回复','depth' => $depth, 'max_depth' => $args['max_depth']))) ?>
         </div>
+   </div>
 <?php } ?>
